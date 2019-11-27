1e3dc2722c6bf61a745cf5bcc77d74d4dc35490b
vilinicz <v.ilinicz@gmail.com>
2019-11-28 02:15:38 +0300

    add dist

---
+++ client/dist/precache-manifest.64d3fc6fbde86f16b36df0194fa712c8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "992f2b901fe4c5bd3b69",
+    "url": "/css/app.3bd7384c.css"
+  },
+  {
+    "revision": "226efd2cd892d17b8ec16036d545848b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "992f2b901fe4c5bd3b69",
+    "url": "/js/app.699e6e19.js"
+  },
+  {
+    "revision": "1bdae035d6f58b0df5fa",
+    "url": "/js/chunk-vendors.ac0972be.js"
+  },
+  {
+    "revision": "a3b77eeb6a16462cb025d0ed4ce244a7",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
