4b9621d3c87b69a0157d4ef165844f8458744122
vilinicz <v.ilinicz@gmail.com>
2019-12-16 01:41:27 +0300

    try one more time

--- camera.py
+++ camera.py
@@ -124,9 +124,12 @@ def open_stream(ctype, url, lat, width, height):
                 'videoconvert ! appsink drop=true sync=false').format(url, lat,
                                                                       width,
                                                                       height)
-    gst_usb = ('v4l2src device=/dev/video{} ! '
-               'video/x-raw, width=(int){}, height=(int){} ! '
-               'videoconvert ! appsink').format(url, width, height)
+
+    gst_usb = 'v4l2src device=/dev/video{} ! video/x-raw, width=(int){}, ' \
+              'height=(int){}, framerate=(fraction){}/1 ! videoconvert !  ' \
+              'video/x-raw, , format=(string)BGR ! appsink'.format(url,
+                                                                   width,
+                                                                   height, 30)
 
     if platform.machine() == "aarch64":
         print("Running on Jetson")
